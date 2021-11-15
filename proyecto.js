13a26a0ceaee261f3a20629b074d95e4b1074d95
JavierPascal <A01375925@itesm.mx>
2021-11-15 17:46:30 -0600

    hotfix

--- proyecto.js
+++ proyecto.js
@@ -28,7 +28,7 @@ let objHammer = {obj:'../3dModels/mjolnirOBJ.obj',  mtl:'../3dModels/mjolnirOBJ.
 let objBox = {obj:'../3dModels/aqua.obj', mtl:'../3dModels/aqua.obj'};
 
 let objBike = {obj: '../3dModels/Tron/bike.obj', mtl: '../3dModels/Tron/bike.mtl'}
-let objRickMorty = {obj: '../3dModels/Room/OBJ/Room #1.obj', mtl: '../3dModels/RickMorty/Portal_guns_and_charging_station.mtl'}
+let objRoom = {obj: '../3dModels/Room/OBJ/Room.obj', mtl: '../3dModels/Room/OBJ/Room.mtl'}
 let objLightBulb = {obj: '../3dModels/Light Bulb/LightBulb_.obj', mtl: '../3dModels/Light Bulb/LightBulb_.mtl'}
 
 let objDesk = {obj: '../3dModels/Desk/Helsinki_Office_35_Desk_System_by_Desalto.obj', mtl: '../3dModels/Desk/Helsinki_Office_35_Desk_System_by_Desalto.mtl'}
@@ -568,7 +568,7 @@ function createScene(canvas)
     loadObjBox(objBox,objectList );
 
     loadObjBike(objBike, objectList);
-    loadRoom(objRickMorty, objectList);
+    loadRoom(objRoom, objectList);
     loadObjLightBulb(objLightBulb, objectList);
 
     // Furniture
